065d8863b6f95e531d8439b0a89275c18e452a8c
Nelson Fernandes <invoke.std@gmail.com>
2024-07-11 07:39:32 +0100

    Solução do Grid/List View

--- src/components/ProductGrid/ProductListCard/ProductListCard.tsx
+++ src/components/ProductGrid/ProductListCard/ProductListCard.tsx
@@ -1,36 +1,33 @@
 import styles from './ProductListCard.module.css'
-import productImg from '../../assets/products/product-cover-1.png'
 
 
-function ProductListCard() {
+function ProductListCard(props) {
+    const { productName,
+        image,
+        description,
+        oldPrice,
+        currentPrice,
+        variantColors } = props
+
     return (
         <div className={styles.productCard}>
-            <img src={productImg} alt="product name" />
+            <img src={image} alt="product name" />
             <div className={styles.info}>
-                <div className={styles.content}>
-                    <h3 className={styles.name}>Graphic Design</h3>
-                    <p className={styles.description}>English Department</p>
-                </div>
-
-                <div className={styles.variations}>
-                    <h4>Color available:</h4>
-                    <div className={styles.colors}>
-                        <span style={{ backgroundColor: "#23A6F0" }}></span>
-                        <span style={{ backgroundColor: "#23856D" }}></span>
-                        <span style={{ backgroundColor: "#E77C40" }}></span>
-                        <span style={{ backgroundColor: "#252B42" }}></span>
-                    </div>
-                </div>
-
-            </div>
-            <div className={styles.actions}>
+                <h3 className={styles.name}>{productName}</h3>
+                <p className={styles.description}>{description}</p>
                 <div className={styles.price}>
-                    <span className={styles.oldPrice}>16.48€</span>
-                    <span className={styles.currentPrice}>6.48€</span>
+                    <span className={styles.oldPrice}>{oldPrice}€</span>
+                    <span className={styles.currentPrice}>{currentPrice}€</span>
                 </div>
-                <button>Add to cart</button>
+                <div className={styles.colors}>
+                    {
+                        //Expressoes .map??? 
+                        variantColors.map((color) => {
+                            return <span style={{ backgroundColor: color }}></span>
+                        })
+                    }
+                </div>
             </div>
-
         </div>
     )
 }
